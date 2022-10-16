19f77d125685b24c33ae1a18eb99d18f67d14ccb
kano-o <steen4145@gmail.com>
2022-10-16 19:15:20 +0200

    changed language to english and outputs text now

--- scraper.js
+++ scraper.js
@@ -11,10 +11,11 @@ async function scrapeLoot(url) {
         }
 
         const browser = await puppeteer.launch({
-            headless: true,
+            headless: false,
             args: [
                 '--no-sandbox', '--window-size=1920,8000'
             ],
+
             defaultViewport: {
                 width:1920,
                 height:8000
@@ -26,24 +27,37 @@ async function scrapeLoot(url) {
         page.once('load', () => console.log('Page loaded!'));
         await page.goto(url, {waitUntil: "networkidle0"});
 
-        console.log('Hit wait for selector')
+
+
+        console.log('Hit wait for selector');
         const test = await page.waitForSelector(".swiper-slide");
 
-        await (await page.$('.tw-button--amazon')).click();
+        await (await page.$('.tw-button--amazon')).click({waitUntil: "networkidle0"});
 
-        //await page.screenshot({path:'screenshots/test.png', fullPage:true})
+        await (await page.$('.language-selector__dropdown-arrow')).click({waitUntil: "networkidle0"});
+        console.log('language dropdown open');
+
+        await page.screenshot({path:'screenshots/test1.png', fullPage:true});
+
+        // await browser.close();
+
+        await (await page.$('[data-language="en-US"]')).click();
+        console.log('language en-US selected');
+
+        await page.waitForNavigation({waitUntil: 'networkidle0'});
+
+        await page.screenshot({path:'screenshots/test2.png', fullPage:true});
 
         console.log('finished waiting for selector');
 
-        const el = await page.$$('.offer-list__content__grid > div.tw-block > div > div > div > a ');
+        const el = await page.$$('.offer-list__content__grid > div.tw-block > div > div > div > a > div > div > div > div.item-card-details > div');
 
 
         console.log(el.length);
 
 
         for (const [i, elElement] of el.entries()) {
-            const href = await elElement.getProperty('href');
-
+            const href = await elElement.getProperty('textContent')
             console.log(await href.jsonValue() + ',');
             //await elElement.screenshot({path:'screenshots/tw-card' + i + '.png'});
         }
